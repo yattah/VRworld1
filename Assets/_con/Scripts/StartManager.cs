a6e199f2eb2be7c511c9f9400746fe0f4c06d529
Yattah <connoro@gmail.com>
2016-08-03 14:51:21 -0400

    initial commit

---
+++ Assets/_con/Scripts/StartManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using Valve.VR;
+
+public class StartManager : MonoBehaviour {
+
+	public Transform vrCam, fpsCam;
+
+	public GameObject FPSCamera;
+
+	public Vector3 spawnPoint, SpawnPoint_VR;
+
+	private Transform mainCam;
+
+	private bool vrIsActive;
+
+	void Awake () {
+		#if UNITY_EDITOR_WIN
+		mainCam = instantiate(vrCam);
+		mainCam.transform.localPosition = spawnPoint_VR;
+		#endif
+
+		#if UNITY_EDITOR_OSX
+		mainCam = Instantiate(fpsCam);
+		mainCam.transform.localPosition = spawnPoint;
+		#endif
+
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
